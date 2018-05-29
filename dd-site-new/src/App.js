bc9dc1b3efecc79b0875cb5b040ce65dd5310d68
Mattias Eyram <mattiaseyram@gmail.com>
2018-05-29 15:29:50 -0400

    cra boiler

--- dd-site-new/src/App.js
+++ dd-site-new/src/App.js
@@ -1,19 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'react-router-redux';
+import store, { history } from './store';
+
+// Styles
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
+// routes
+import routes from './routes';
+
+// common components
+import Header from './common/components/Header'
+import Footer from './common/components/Footer'
+
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <div className="App">
+            <Header />
+            <div className="wrap">
+              {routes}
+            </div>
+            <Footer />
+          </div>
+        </ConnectedRouter>
+      </Provider>
     );
   }
 }
